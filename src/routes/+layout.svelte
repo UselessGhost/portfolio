14e86ee5bcea3ed23fa438946da96c49df5cd229
May <6723503+UselessGhost@users.noreply.github.com>
2026-05-20 13:40:47 -0700

    oh oops. delete default ugly favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>	
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	//import favicon from '$lib/assets/favicon.svg';
 
 	import Modal from "$lib/modal.svelte"
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+</svelte:head>
 
 <Modal>
 	{@render children?.()}
